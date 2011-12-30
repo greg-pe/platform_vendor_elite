93443c30295e114afe451e4b827164e80d343547
Greg <projecteliteroms@gmail.com>
2011-12-30 18:25:41 -0500

    Fix accidental deletion

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,7 +1,9 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build
 
+# Additional properties
 PRODUCT_VERSION_MAJOR = 8
 PRODUCT_VERSION_MINOR = 0
 
+PRODUCT_PROPERTY_OVERRIDES += \
 ro.romversion=ProjectElite-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-NIGHTLY-$(shell date +%m%d%Y)
