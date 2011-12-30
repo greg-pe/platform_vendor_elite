eda4ffde4bacdd7529fcfb26659b344d17b06cfa
Greg <projecteliteroms@gmail.com>
2011-12-29 20:14:41 -0500

    Change Rom Version formatting and name

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build
 
+PRODUCT_VERSION_MAJOR = 8
+PRODUCT_VERSION_MINOR = NIGHTLY
 
-# Additional properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.romversion=version 8
+ro.romversion=ProjectElite-$(PRODUCT_VERSION_MAJOR)-$(shell date +%m%d%Y).$(PRODUCT_VERSION_MINOR)
